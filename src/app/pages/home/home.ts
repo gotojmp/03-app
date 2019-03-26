d93f4b754f137ecaa8cd1130d449df3c5aa12493
David Young <david@ad4.com.cn>
2019-03-26 19:11:56 +0800

    make homepage

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -7,126 +7,157 @@ import { ConferenceData } from '../../providers/conference-data';
 import { UserData } from '../../providers/user-data';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html',
-  styleUrls: ['./home.scss'],
+    selector: 'page-home',
+    templateUrl: 'home.html',
+    styleUrls: ['./home.scss'],
 })
 export class HomePage implements OnInit {
-  // Gets a reference to the list element
-  @ViewChild('scheduleList') scheduleList: IonList;
+    // Gets a reference to the list element
+    @ViewChild('scheduleList') scheduleList: IonList;
 
-  dayIndex = 0;
-  queryText = '';
-  segment = 'all';
-  excludeTracks: any = [];
-  shownSessions: any = [];
-  groups: any = [];
-  confDate: string;
+    dayIndex = 0;
+    queryText = '';
+    segment = 'all';
+    excludeTracks: any = [];
+    shownSessions: any = [];
+    groups: any = [];
+    confDate: string;
+    itemGroups: any[] = [];
 
-  constructor(
-    public alertCtrl: AlertController,
-    public confData: ConferenceData,
-    public loadingCtrl: LoadingController,
-    public modalCtrl: ModalController,
-    public router: Router,
-    public toastCtrl: ToastController,
-    public user: UserData
-  ) { }
+    constructor(
+        public alertCtrl: AlertController,
+        public confData: ConferenceData,
+        public loadingCtrl: LoadingController,
+        public modalCtrl: ModalController,
+        public router: Router,
+        public toastCtrl: ToastController,
+        public user: UserData
+    ) { }
 
-  ngOnInit() {
-    // this.app.setTitle('Schedule');
-    this.updateSchedule();
-  }
-
-  updateSchedule() {
-    // Close any open sliding items when the schedule updates
-    if (this.scheduleList) {
-      this.scheduleList.closeSlidingItems();
+    ngOnInit() {
+        // this.app.setTitle('Schedule');
+        this.updateSchedule();
     }
 
-    this.confData.getTimeline(this.dayIndex, this.queryText, this.excludeTracks, this.segment).subscribe((data: any) => {
-      this.shownSessions = data.shownSessions;
-      this.groups = data.groups;
-    });
-  }
-
-  async presentFilter() {
-    const modal = await this.modalCtrl.create({
-      component: ScheduleFilterPage,
-      componentProps: { excludedTracks: this.excludeTracks }
-    });
-    await modal.present();
-
-    const { data } = await modal.onWillDismiss();
-    if (data) {
-      this.excludeTracks = data;
-      this.updateSchedule();
-    }
-  }
-
-  async addFavorite(slidingItem: HTMLIonItemSlidingElement, sessionData: any) {
-    if (this.user.hasFavorite(sessionData.name)) {
-      // woops, they already favorited it! What shall we do!?
-      // prompt them to remove it
-      this.removeFavorite(slidingItem, sessionData, 'Favorite already added');
-    } else {
-      // remember this session as a user favorite
-      this.user.addFavorite(sessionData.name);
-
-      // create an alert instance
-      const alert = await this.alertCtrl.create({
-        header: 'Favorite Added',
-        buttons: [{
-          text: 'OK',
-          handler: () => {
-            // close the sliding item
-            slidingItem.close();
-          }
-        }]
-      });
-      // now present the alert on top of all other content
-      await alert.present();
+    ionViewDidEnter() {
+        this.itemGroups = [
+            {
+                title: 'Meat',
+                items: []
+            },
+            {
+                title: 'Fruit',
+                items: []
+            },
+            {
+                title: 'Seafood',
+                items: []
+            },
+            {
+                title: 'Rice',
+                items: []
+            },
+            {
+                title: 'Snack',
+                items: []
+            }
+        ];
+        this.confData.getSpeakers().subscribe((speakers: any[]) => {
+            this.itemGroups.forEach(group => {
+                group.items = speakers;
+            });
+        });
     }
 
-  }
-
-  async removeFavorite(slidingItem: HTMLIonItemSlidingElement, sessionData: any, title: string) {
-    const alert = await this.alertCtrl.create({
-      header: title,
-      message: 'Would you like to remove this session from your favorites?',
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: () => {
-            // they clicked the cancel button, do not remove the session
-            // close the sliding item and hide the option buttons
-            slidingItem.close();
-          }
-        },
-        {
-          text: 'Remove',
-          handler: () => {
-            // they want to remove this session from their favorites
-            this.user.removeFavorite(sessionData.name);
-            this.updateSchedule();
-
-            // close the sliding item and hide the option buttons
-            slidingItem.close();
-          }
+    updateSchedule() {
+        // Close any open sliding items when the schedule updates
+        if (this.scheduleList) {
+            this.scheduleList.closeSlidingItems();
         }
-      ]
-    });
-    // now present the alert on top of all other content
-    await alert.present();
-  }
 
-  async openSocial(network: string, fab: HTMLIonFabElement) {
-    const loading = await this.loadingCtrl.create({
-      message: `Posting to ${network}`,
-      duration: (Math.random() * 1000) + 500
-    });
-    await loading.present();
-    await loading.onWillDismiss();
-    fab.close();
-  }
+        this.confData.getTimeline(this.dayIndex, this.queryText, this.excludeTracks, this.segment).subscribe((data: any) => {
+            this.shownSessions = data.shownSessions;
+            this.groups = data.groups;
+        });
+    }
+
+    async presentFilter() {
+        const modal = await this.modalCtrl.create({
+            component: ScheduleFilterPage,
+            componentProps: { excludedTracks: this.excludeTracks }
+        });
+        await modal.present();
+
+        const { data } = await modal.onWillDismiss();
+        if (data) {
+            this.excludeTracks = data;
+            this.updateSchedule();
+        }
+    }
+
+    async addFavorite(slidingItem: HTMLIonItemSlidingElement, sessionData: any) {
+        if (this.user.hasFavorite(sessionData.name)) {
+            // woops, they already favorited it! What shall we do!?
+            // prompt them to remove it
+            this.removeFavorite(slidingItem, sessionData, 'Favorite already added');
+        } else {
+            // remember this session as a user favorite
+            this.user.addFavorite(sessionData.name);
+
+            // create an alert instance
+            const alert = await this.alertCtrl.create({
+                header: 'Favorite Added',
+                buttons: [{
+                    text: 'OK',
+                    handler: () => {
+                        // close the sliding item
+                        slidingItem.close();
+                    }
+                }]
+            });
+            // now present the alert on top of all other content
+            await alert.present();
+        }
+
+    }
+
+    async removeFavorite(slidingItem: HTMLIonItemSlidingElement, sessionData: any, title: string) {
+        const alert = await this.alertCtrl.create({
+            header: title,
+            message: 'Would you like to remove this session from your favorites?',
+            buttons: [
+                {
+                    text: 'Cancel',
+                    handler: () => {
+                        // they clicked the cancel button, do not remove the session
+                        // close the sliding item and hide the option buttons
+                        slidingItem.close();
+                    }
+                },
+                {
+                    text: 'Remove',
+                    handler: () => {
+                        // they want to remove this session from their favorites
+                        this.user.removeFavorite(sessionData.name);
+                        this.updateSchedule();
+
+                        // close the sliding item and hide the option buttons
+                        slidingItem.close();
+                    }
+                }
+            ]
+        });
+        // now present the alert on top of all other content
+        await alert.present();
+    }
+
+    async openSocial(network: string, fab: HTMLIonFabElement) {
+        const loading = await this.loadingCtrl.create({
+            message: `Posting to ${network}`,
+            duration: (Math.random() * 1000) + 500
+        });
+        await loading.present();
+        await loading.onWillDismiss();
+        fab.close();
+    }
 }
--- src/app/pages/speaker-list/speaker-list.ts
+++ src/app/pages/speaker-list/speaker-list.ts
@@ -6,89 +6,89 @@ import { ActionSheetController } from '@ionic/angular';
 import { ConferenceData } from '../../providers/conference-data';
 
 @Component({
-  selector: 'page-speaker-list',
-  templateUrl: 'speaker-list.html',
-  styleUrls: ['./speaker-list.scss'],
+    selector: 'page-speaker-list',
+    templateUrl: 'speaker-list.html',
+    styleUrls: ['./speaker-list.scss'],
 })
 export class SpeakerListPage {
-  speakers: any[] = [];
+    speakers: any[] = [];
 
-  constructor(
-    public actionSheetCtrl: ActionSheetController,
-    public confData: ConferenceData,
-    public inAppBrowser: InAppBrowser,
-    public router: Router
-  ) {}
+    constructor(
+        public actionSheetCtrl: ActionSheetController,
+        public confData: ConferenceData,
+        public inAppBrowser: InAppBrowser,
+        public router: Router
+    ) {}
 
-  ionViewDidEnter() {
-    this.confData.getSpeakers().subscribe((speakers: any[]) => {
-      this.speakers = speakers;
-    });
-  }
+    ionViewDidEnter() {
+        this.confData.getSpeakers().subscribe((speakers: any[]) => {
+            this.speakers = speakers;
+        });
+    }
 
-  goToSpeakerTwitter(speaker: any) {
-    this.inAppBrowser.create(
-      `https://twitter.com/${speaker.twitter}`,
-      '_blank'
-    );
-  }
+    goToSpeakerTwitter(speaker: any) {
+        this.inAppBrowser.create(
+            `https://twitter.com/${speaker.twitter}`,
+            '_blank'
+        );
+    }
 
-  async openSpeakerShare(speaker: any) {
-    const actionSheet = await this.actionSheetCtrl.create({
-      header: 'Share ' + speaker.name,
-      buttons: [
-        {
-          text: 'Copy Link',
-          handler: () => {
-            console.log(
-              'Copy link clicked on https://twitter.com/' + speaker.twitter
-            );
-            if (
-              (window as any)['cordova'] &&
-              (window as any)['cordova'].plugins.clipboard
-            ) {
-              (window as any)['cordova'].plugins.clipboard.copy(
-                'https://twitter.com/' + speaker.twitter
-              );
-            }
-          }
-        },
-        {
-          text: 'Share via ...'
-        },
-        {
-          text: 'Cancel',
-          role: 'cancel'
-        }
-      ]
-    });
+    async openSpeakerShare(speaker: any) {
+        const actionSheet = await this.actionSheetCtrl.create({
+            header: 'Share ' + speaker.name,
+            buttons: [
+                {
+                    text: 'Copy Link',
+                    handler: () => {
+                        console.log(
+                            'Copy link clicked on https://twitter.com/' + speaker.twitter
+                        );
+                        if (
+                            (window as any)['cordova'] &&
+                            (window as any)['cordova'].plugins.clipboard
+                        ) {
+                            (window as any)['cordova'].plugins.clipboard.copy(
+                                'https://twitter.com/' + speaker.twitter
+                            );
+                        }
+                    }
+                },
+                {
+                    text: 'Share via ...'
+                },
+                {
+                    text: 'Cancel',
+                    role: 'cancel'
+                }
+            ]
+        });
 
-    await actionSheet.present();
-  }
+        await actionSheet.present();
+    }
 
-  async openContact(speaker: any) {
-    const mode = 'ios'; // this.config.get('mode');
+    async openContact(speaker: any) {
+        const mode = 'ios'; // this.config.get('mode');
 
-    const actionSheet = await this.actionSheetCtrl.create({
-      header: 'Contact ' + speaker.name,
-      buttons: [
-        {
-          text: `Email ( ${speaker.email} )`,
-          icon: mode !== 'ios' ? 'mail' : null,
-          handler: () => {
-            window.open('mailto:' + speaker.email);
-          }
-        },
-        {
-          text: `Call ( ${speaker.phone} )`,
-          icon: mode !== 'ios' ? 'call' : null,
-          handler: () => {
-            window.open('tel:' + speaker.phone);
-          }
-        }
-      ]
-    });
+        const actionSheet = await this.actionSheetCtrl.create({
+            header: 'Contact ' + speaker.name,
+            buttons: [
+                {
+                    text: `Email ( ${speaker.email} )`,
+                    icon: mode !== 'ios' ? 'mail' : null,
+                    handler: () => {
+                        window.open('mailto:' + speaker.email);
+                    }
+                },
+                {
+                    text: `Call ( ${speaker.phone} )`,
+                    icon: mode !== 'ios' ? 'call' : null,
+                    handler: () => {
+                        window.open('tel:' + speaker.phone);
+                    }
+                }
+            ]
+        });
 
-    await actionSheet.present();
-  }
+        await actionSheet.present();
+    }
 }
